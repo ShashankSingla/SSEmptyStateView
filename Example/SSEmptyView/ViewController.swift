21bacc1868c07dfa0a22263c283aabb53ac78545
Shashank kumar <singla.26july@gmail.com>
2019-09-07 12:14:33 +0530

    Added example to use files

--- Example/SSEmptyView/ViewController.swift
+++ Example/SSEmptyView/ViewController.swift
@@ -7,18 +7,22 @@
 //
 
 import UIKit
+import SSEmptyView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let view = SSEmptyView(frame: self.view.frame, image: "icn_notification", titleText: "Test", subText: "This is the emptyview.")
+        self.view.addSubview(view)
+        view.showRefreshButton = true
+        view.delegate = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: EmptyViewDelegate {
+    func viewDidRefreshTapped() {
+        print("View refreshed........")
     }
-
 }
 
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import SSEmptyView
 
 class Tests: XCTestCase {
     
